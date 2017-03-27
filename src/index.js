5e91e1a818bfd445505a76af215d71b2e0dae4f1
vitalets <vitalets@yandex-team.ru>
2017-03-27 22:16:07 +0300

    add enabled option

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const BinaryVisitor = require('./binary-visitor');
 module.exports = function() {
   return {
     visitor: {
-      BinaryExpression(path) {
-        new BinaryVisitor(path).run();
+      BinaryExpression(path, state) {
+        if (state.opts.enabled || state.opts.enabled === undefined) {
+          new BinaryVisitor(path).run();
+        }
       }
     }
   };
