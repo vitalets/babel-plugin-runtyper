6ef451ad4ecd1a449d9d815ef2e8d062a70c3190
vitalets <vitalets@yandex-team.ru>
2017-03-28 11:34:57 +0300

    add option allowStringNumberConcat

--- src/binary-assertions/plus.js
+++ src/binary-assertions/plus.js
@@ -8,16 +8,23 @@ const OPERATORS = {
 
 const TPL = `
   var f = VALUE_INFO;
+  var types = ['number', 'string'];
+  if (types.indexOf(typeof a) === -1 || types.indexOf(typeof b) === -1) {
+    throw new Error('Plus operation should be used for numbers or strings: ' + f(a) + ' ' + OPERATOR + ' ' + f(b));
+  }
+  ASSERT_EQUAL_TYPES
+`;
+
+const EQUAL_TYPES_TPL = `
   if (typeof a !== typeof b) {
     throw new Error('Plus operation with different types: ' + f(a) + ' ' + OPERATOR + ' ' + f(b));
-  }
-  if (['number', 'string'].indexOf(typeof a) === -1) {
-    throw new Error('Plus operation should be used for numbers or strings: ' + f(a) + ' ' + OPERATOR + ' ' + f(b));
   }
 `;
 
 module.exports = class PlusAssertion extends BinaryAssertion {
   constructor(options) {
-    super(options, OPERATORS, TPL);
+    const assertEqualTypes = options.allowStringNumberConcat ? '' : EQUAL_TYPES_TPL;
+    const tpl = TPL.replace('ASSERT_EQUAL_TYPES', assertEqualTypes.trim());
+    super(options, OPERATORS, tpl);
   }
 };
--- test/specs/plus.spec.js
+++ test/specs/plus.spec.js
@@ -5,15 +5,15 @@ describe('plus', function () {
     });
 
     it('throws for (number, boolean)', function () {
-      assert.throws(() => samples.addVars(1, true), 'Plus operation with different types: 1 (number) + true (boolean)');
+      assert.throws(() => samples.addVars(1, true), 'Plus operation should be used for numbers or strings: 1 (number) + true (boolean)');
     });
 
     it('throws for (number, null)', function () {
-      assert.throws(() => samples.addVars(1, null), 'Plus operation with different types: 1 (number) + null');
+      assert.throws(() => samples.addVars(1, null), 'Plus operation should be used for numbers or strings: 1 (number) + null');
     });
 
     it('throws for (number, undefined)', function () {
-      assert.throws(() => samples.addVars(1, undefined), 'Plus operation with different types: 1 (number) + undefined');
+      assert.throws(() => samples.addVars(1, undefined), 'Plus operation should be used for numbers or strings: 1 (number) + undefined');
     });
   });
 
